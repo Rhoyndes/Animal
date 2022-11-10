fc02ea958b90da27d00aca534ae6dd4ed035fd83
Daniel <101649846+Rhoyndes@users.noreply.github.com>
2022-11-10 13:00:17 +0100

    initial commit

---
+++ animal.js
@@ -0,0 +1,578 @@
+export const positionPartBody = [
+    [  
+        {
+            left: '29%',
+            top:'38%',
+            height: '4vh', 
+            width: '4vh', 
+            borderRadius: '1vh'
+        }, 
+        {
+            left: '35%', 
+            top: '35%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '37%', 
+            top: '22%',
+            height: '4vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%', 
+            top: '50%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '50%',
+            top: '50%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '57.5%',
+            top: '50%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '65%',
+            top: '40%',
+            height: '4vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '57.5%',
+            top: '65.5%',
+            height: '6vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%',
+            top: '65.5%',
+            height: '6vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%',
+            top: '65.5%',
+            height: '6vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        }
+    ],
+    [
+        {
+            left: '30%',
+            top:'38%',
+            height: '4vh', 
+            width: '2vh', 
+            borderRadius: '1vh'
+        }, 
+        {
+            left: '35%', 
+            top: '35%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '35%', 
+            top: '22%',
+            height: '4vh',
+            width: '2vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%', 
+            top: '50%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '50%',
+            top: '57.5%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '57.5%',
+            top: '65%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '65%',
+            top: '69%',
+            height: '4vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '29.5%',
+            top:'38%',
+            height: '2vh', 
+            width: '2vh', 
+            borderRadius: '1vh'
+        },
+        {
+            left: '51%',
+            top: '70%',
+            height: '4vh',
+            width: '6vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '40%',
+            top: '66.5%',
+            height: '7vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        }
+    ],
+    [  
+        {
+            left: '33%',
+            top:'31.5%',
+            height: '4vh', 
+            width: '8vh', 
+            borderRadius: '1vh'
+        }, 
+        {
+            left: '39.5%', 
+            top: '27.5%',
+            height: '8vh',
+            width: '6vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '40.5%', 
+            top: '12%',
+            height: '6vh',
+            width: '2vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '47.5%', 
+            top: '57.5%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '55%',
+            top: '57.5%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '62.5%',
+            top: '57.5%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '67.5%',
+            top: '52%',
+            height: '2vh',
+            width: '2vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '62.5%',
+            top: '77%',
+            height: '10vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '47.5%',
+            top: '77%',
+            height: '10vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '43.5%',
+            top: '42.5%',
+            height: '6vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        }
+    ],
+    [  
+        {
+            left: '33%',
+            top:'57.5%',
+            height: '15vh', 
+            width: '3vh', 
+            borderRadius: '5vh'
+        }, 
+        {
+            left: '39%', 
+            top: '40%',
+            height: '11vh',
+            width: '9vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42%', 
+            top: '26%',
+            height: '2vh',
+            width: '5vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '47.5%', 
+            top: '45%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '55%',
+            top: '47.5%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '62.5%',
+            top: '52.5%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '67%',
+            top: '52%',
+            height: '5vh',
+            width: '1vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '62.5%',
+            top: '68.5%',
+            height: '6vh',
+            width: '6vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '46.5%',
+            top: '65%',
+            height: '10vh',
+            width: '6vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '47.5%',
+            top: '65.5%',
+            height: '6vh',
+            width: '2vh',
+            borderRadius: '1vh'
+        }
+    ],
+    [  
+        {
+            left: '50%', 
+            top: '80%',
+            height: '3vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        }, 
+        {
+            left: '39%', 
+            top: '50%',
+            height: '32vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '67%',
+            top: '50%',
+            height: '18vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '50%', 
+            top: '68%',
+            height: '8vh',
+            width: '5vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '50%',
+            top: '50%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '50%',
+            top: '37%',
+            height: '3vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '61%', 
+            top: '50%',
+            height: '32vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '55.5%',
+            top: '50%',
+            height: '18vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '44.5%',
+            top: '50%',
+            height: '18vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '33%',
+            top: '50%',
+            height: '18vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        }
+    ],
+    [
+        {
+            left: '28.5%',
+            top:'50%',
+            height: '2vh', 
+            width: '2vh', 
+            borderRadius: '1vh'
+        }, 
+        {
+            left: '32%', 
+            top: '50%',
+            height: '5vh',
+            width: '5vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '63.5%', 
+            top: '50%',
+            height: '12vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '38%', 
+            top: '50%',
+            height: '8vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '47%',
+            top: '50%',
+            height: '11vh',
+            width: '11vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '55.5%',
+            top: '50%',
+            height: '7vh',
+            width: '7vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '60.5%',
+            top: '50%',
+            height: '5vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '67%',
+            top: '50%',
+            height: '18vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '71%',
+            top: '50%',
+            height: '24vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '45%',
+            top: '64.5%',
+            height: '2vh',
+            width: '6vh',
+            borderRadius: '1vh'
+        }
+    ],
+    [  
+        {
+            left: '32.5%',
+            top:'49%',
+            height: '3vh', 
+            width: '3vh', 
+            borderRadius: '1vh'
+        }, 
+        {
+            left: '36.5%', 
+            top: '47%',
+            height: '5vh',
+            width: '5vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '29.5%', 
+            top: '49%',
+            height: '0.5vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%', 
+            top: '49.5%',
+            height: '4vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '50%',
+            top: '52%',
+            height: '4vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '57.5%',
+            top: '48%',
+            height: '4vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '64%',
+            top: '44%',
+            height: '3vh',
+            width: '6vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '57.5%',
+            top: '65.5%',
+            height: '0vh',
+            width: '0vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%',
+            top: '65.5%',
+            height: '0vh',
+            width: '0vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%',
+            top: '65.5%',
+            height: '0vh',
+            width: '0vh',
+            borderRadius: '1vh'
+        }
+    ],
+    [  
+        {
+            left: '29%',
+            top:'38%',
+            height: '0vh', 
+            width: '0vh', 
+            borderRadius: '1vh'
+        }, 
+        {
+            left: '35%', 
+            top: '35%',
+            height: '0vh',
+            width: '0vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '37%', 
+            top: '22%',
+            height: '0vh',
+            width: '0vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '35.5%', 
+            top: '50%',
+            height: '20vh',
+            width: '20vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '50.5%',
+            top: '59%',
+            height: '12vh',
+            width: '12vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '59%',
+            top: '62.5%',
+            height: '6vh',
+            width: '6vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '66%',
+            top: '58%',
+            height: '4vh',
+            width: '8vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '71.5%',
+            top: '57%',
+            height: '8vh',
+            width: '3vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '75%',
+            top: '56%',
+            height: '14vh',
+            width: '4vh',
+            borderRadius: '1vh'
+        },
+        {
+            left: '42.5%',
+            top: '65.5%',
+            height: '0vh',
+            width: '0vh',
+            borderRadius: '1vh'
+        }
+    ]
+]
---
+++ cinema.js
@@ -0,0 +1,150 @@
+import { positionPartBody } from './animal.js'
+
+class Cinema{
+    constructor(){
+        this.ghostBody = document.querySelector('.body')
+        this.ghostBodyAnimation = document.querySelector('.body__chest')
+        this.ghostRightEye = document.querySelector('.body__head-eyeRight')
+        this.ghostLeftEye = document.querySelector('.body__head-eyeLeft')
+
+
+        this.panelButtons = [...document.querySelectorAll('.panel-button-icon')]
+        this.panelButtonsTop = [...document.querySelectorAll('.panel-button-icon-top')]
+        this.partsOfBody = [...document.querySelectorAll('.screen__animal-el')]
+
+        this.scene = document.querySelector('.screen__animal-off')
+        this.loadingNumber = document.querySelector('.screen__animal-intro-number')
+        this.sceneLeft = [...document.querySelectorAll('.screen__animal-intro div div')]
+        this.loadingSceneOff = document.querySelector('.screen__animal-intro')
+
+        this.moveAccess = true
+        this.numerActive = 0
+        this.positionLeft = 0
+        this.timeLeftNumber = 3
+
+        this.sceneOn = true
+
+        this.init()
+    }
+    init(){
+        this.intro()
+        this.keyAction()
+        this.keyCreateAnimal()
+    }
+    intro = () => {
+        this.ghostBody.style.opacity = '1'
+        this.ghostBody.style.left = '0'
+        this.ghostBodyAnimation.style.animation = 'none'
+        this.ghostBodyAnimation.style.borderRadius = `50% 50% 95% 5% / 18% 18% 82% 82%`
+        this.ghostRightEye.style.display = 'block'
+        setTimeout(() => {
+            this.ghostBodyAnimation.style.animation = 'ghostMoving .7s ease-in-out infinite alternate'
+            this.ghostRightEye.style.display = 'none'
+        }, 500)
+    }
+    keyAction = () => {
+        document.addEventListener('keyup', event => {
+            if(this.moveAccess){
+                if(event.key === 'ArrowRight' && this.positionLeft < 90){  
+                    this.moveAccess = false 
+                    this.numerActive++
+                    this.positionLeft += 10
+                    this.ghostRightEye.style.display = 'block'
+                    this.ghostBody.style.left = `${this.positionLeft}%`
+                    this.ghostBodyAnimation.style.animation = 'none'
+                    this.ghostBodyAnimation.style.borderRadius = `50% 50% 95% 5% / 18% 18% 82% 82%`
+                    setTimeout(() => {
+                        this.ghostBodyAnimation.style.animation = 'ghostMoving .7s ease-in-out infinite alternate'
+                        this.ghostRightEye.style.display = 'none'
+                        this.moveAccess = true
+                    }, 500)
+                    this.activeButton()
+                }
+                if(event.key === 'ArrowLeft' && this.positionLeft > 0){
+                    this.moveAccess = false
+                    this.numerActive--
+                    this.positionLeft -= 10
+                    this.ghostLeftEye.style.display = 'block'
+                    this.ghostBody.style.left = `${this.positionLeft}%`
+                    this.ghostBodyAnimation.style.animation = 'none'
+                    this.ghostBodyAnimation.style.borderRadius = `50% 50% 5% 95% / 18% 18% 82% 82%`
+                    setTimeout(() => {
+                        this.ghostBodyAnimation.style.animation = 'ghostMoving .7s ease-in-out infinite alternate'
+                        this.ghostLeftEye.style.display = 'none'
+                        this.moveAccess = true
+                    }, 500)
+                    this.activeButton()
+                }
+            }
+        })
+    }
+    activeButton = () => {
+        this.panelButtons.forEach(btn => {
+            btn.style.border = ''
+        })
+        if(this.numerActive > 0 && this.numerActive < 9){
+            this.panelButtons[this.numerActive - 1].style.border = '2px solid yellow'
+        }
+    }
+    createAnimal = () => {
+        this.partsOfBody.forEach((part, id) => {
+            part.style.opacity = '1'
+            part.style.left = positionPartBody[this.numerActive - 1][id].left
+            part.style.top = positionPartBody[this.numerActive - 1][id].top
+            part.style.height = positionPartBody[this.numerActive - 1][id].height
+            part.style.borderRadius = positionPartBody[this.numerActive - 1][id].borderRadius
+            part.style.width = positionPartBody[this.numerActive - 1][id].width
+            })
+    }
+    keyCreateAnimal = () => {
+        document.addEventListener('keyup', event => {
+            this.panelButtons.forEach(e => {
+                if(e.style.border !== '' && event.key === 'Enter'){
+                    if(this.sceneOn){
+                        this.scene.style.display = 'none'
+                        setTimeout(() => {
+                            this.sceneOnFunction()
+                        }, 250)
+                    }
+                    this.sceneOn = false
+                    this.panelButtonsTop.forEach(item => {
+                        item.style.width = '90%'
+                        item.style.height = '90%'
+                    })
+                    this.panelButtonsTop[this.numerActive - 1].style.width = 'calc(100% - 2px)'
+                    this.panelButtonsTop[this.numerActive - 1].style.height = 'calc(100% - 2px)'
+                    this.createAnimal()
+                }
+            })
+        })
+    }
+    sceneOnFunction = () => {
+        this.loadingNumber.textContent = this.timeLeftNumber
+        this.sceneLeft[2].style.rotate = '90deg'
+        setTimeout(() => {
+            this.sceneLeft[3].style.rotate = '90deg'
+        }, 200)
+        setTimeout(() => {
+            this.sceneLeft[1].style.rotate = '90deg'
+        }, 400)
+        setTimeout(() => {
+            this.sceneLeft[0].style.rotate = '90deg'
+        }, 600)
+        setTimeout(() => {
+            this.sceneLeft.forEach(item => item.style.rotate = '0deg')
+            this.timeLeftNumber--
+        }, 800)
+       
+        let interval = setTimeout(() => {
+                this.sceneOnFunction()
+            }, 1000)
+        if(this.loadingNumber.textContent == 1){
+            clearInterval(interval)
+            setTimeout(() => {
+                this.loadingSceneOff.style.display = 'none'
+                this.partsOfBody.forEach(part => part.style.display = 'block')
+            }, 800)
+        }
+    }
+}
+const cinema = new Cinema()
